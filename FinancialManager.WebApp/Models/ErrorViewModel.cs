5b206271571b4a6e9773afc71539861ca8dff11a
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2025-05-07 21:52:41 -0300

    Creating Project

---
+++ FinancialManager.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinancialManager.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
